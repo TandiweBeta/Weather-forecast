c917fcad123a38791e901ee75a81291ceadc7e7a
TandiweBeta <tandiwebeta@gmail.com>
2024-05-10 20:50:31 +0200

    added weather forecast details in html from javascript

--- src/app.js
+++ src/app.js
@@ -60,3 +60,29 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
 
 changeCity("Johannesburg");
+displayForecast();
+
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
